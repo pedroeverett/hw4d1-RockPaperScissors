12fe38972117b95869bfe2f4dee32a92bafc6d83
Pedro Everett <pedrojoseeverett@gmail.com>
2017-06-12 18:06:30 +0100

    welcome page and css

--- controller.rb
+++ controller.rb
@@ -3,6 +3,10 @@ require("sinatra/contrib/all")
 require("pry-byebug")
 require_relative("models/rock_paper_scissors.rb")
 
+get '/'do
+  erb(:home)
+end
+
 get '/:choice1/:choice2' do
   game = Game.new(params["choice1"], params["choice2"])
   @result = game.compare()
